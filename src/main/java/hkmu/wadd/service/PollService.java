2e59744e62dc35be5dcd9e47e63e69622b1574dd
Terriuo <118095438+Terriuo@users.noreply.github.com>
2025-04-18 02:48:04 +0800

    Update PollService.java

--- src/main/java/hkmu/wadd/service/PollService.java
+++ src/main/java/hkmu/wadd/service/PollService.java
@@ -1,11 +1,15 @@
-package main.java.hkmu.wadd.service;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package hkmu.wadd.service;
 
 import hkmu.wadd.model.Poll;
 import hkmu.wadd.repository.PollRepository;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.stereotype.Service;
 
 @Service
 public class PollService {
@@ -16,14 +20,14 @@ public class PollService {
     }
 
     public Poll savePoll(Poll poll) {
-        return pollRepository.save(poll);
+        return (Poll)this.pollRepository.save(poll);
     }
 
     public Optional<Poll> getPollById(Long id) {
-        return pollRepository.findById(id);
+        return this.pollRepository.findById(id);
     }
 
     public List<Poll> getAllPolls() {
-        return pollRepository.findAll();
+        return this.pollRepository.findAll();
     }
 }
