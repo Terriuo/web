4b9b645f06af6b6edcf8470aabc12671a6baa29b
Terriuo <118095438+Terriuo@users.noreply.github.com>
2025-04-18 02:45:05 +0800

    Update CommentRepository.java

--- src/main/java/hkmu/wadd/repository/CommentRepository.java
+++ src/main/java/hkmu/wadd/repository/CommentRepository.java
@@ -1,7 +1,18 @@
-package main.java.hkmu.wadd.repository;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package hkmu.wadd.repository;
 
 import hkmu.wadd.model.Comment;
+import hkmu.wadd.model.Lecture;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByUserId(UUID userId);
+
+    List<Comment> findByLecture(Lecture lecture);
 }
