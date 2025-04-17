7a34a3ced53de3279cf4e4871c16105eba03a05a
Terriuo <118095438+Terriuo@users.noreply.github.com>
2025-04-18 02:41:33 +0800

    Update UsersController.java

--- src/main/java/hkmu/wadd/controller/UsersController.java
+++ src/main/java/hkmu/wadd/controller/UsersController.java
@@ -1,33 +1,39 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package hkmu.wadd.controller;
 
 import hkmu.wadd.dao.UsersRepository;
-import hkmu.wadd.model.GuestBookEntry;
 import hkmu.wadd.model.UsersEntry;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.Date;
-import java.util.UUID;
-
 @Controller
-@RequestMapping("/login")
+@RequestMapping({"/login"})
 public class UsersController {
     @Resource
     private UsersRepository usRepo;
 
-    @GetMapping("/register")
+    public UsersController() {
+    }
+
+    @GetMapping({"/register"})
     public ModelAndView signUpForm() {
         return new ModelAndView("SignUp", "entry", new UsersEntry());
     }
 
-    @PostMapping("/register")
+    @PostMapping({"/register"})
     public View signUpForm(@ModelAttribute("entry") UsersEntry usEntry) {
-        usRepo.save(usEntry);
-        return new RedirectView("/Project/login"); // One way to redirect in Spring MVC
+        this.usRepo.save(usEntry);
+        return new RedirectView("/Project/login");
     }
 }
