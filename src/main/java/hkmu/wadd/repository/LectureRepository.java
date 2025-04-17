6708578e98c2b4888f95b412f00494305e60aa20
Terriuo <118095438+Terriuo@users.noreply.github.com>
2025-04-18 02:45:20 +0800

    Update LectureRepository.java

--- src/main/java/hkmu/wadd/repository/LectureRepository.java
+++ src/main/java/hkmu/wadd/repository/LectureRepository.java
@@ -1,4 +1,9 @@
-package main.java.hkmu.wadd.repository;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package hkmu.wadd.repository;
 
 import hkmu.wadd.model.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
