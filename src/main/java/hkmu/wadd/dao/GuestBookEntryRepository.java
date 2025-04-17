6467318c123f27c2c7cf158b487e87fe7081f7d2
Terriuo <118095438+Terriuo@users.noreply.github.com>
2025-04-18 02:41:50 +0800

    Update GuestBookEntryRepository.java

--- src/main/java/hkmu/wadd/dao/GuestBookEntryRepository.java
+++ src/main/java/hkmu/wadd/dao/GuestBookEntryRepository.java
@@ -1,7 +1,12 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package hkmu.wadd.dao;
 
 import hkmu.wadd.model.GuestBookEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GuestBookEntryRepository extends JpaRepository<GuestBookEntry, Long>{
+public interface GuestBookEntryRepository extends JpaRepository<GuestBookEntry, Long> {
 }
