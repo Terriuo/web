642d8e4ac39ee137876571d3799a369a100f73af
Terriuo <118095438+Terriuo@users.noreply.github.com>
2025-04-18 02:45:39 +0800

    Update PollOptionRepository.java

--- src/main/java/hkmu/wadd/repository/PollOptionRepository.java
+++ src/main/java/hkmu/wadd/repository/PollOptionRepository.java
@@ -1,7 +1,15 @@
-package main.java.hkmu.wadd.repository;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
+package hkmu.wadd.repository;
+
+import hkmu.wadd.model.Poll;
 import hkmu.wadd.model.PollOption;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PollOptionRepository extends JpaRepository<PollOption, Long> {
+    List<PollOption> findByPoll(Poll poll);
 }
